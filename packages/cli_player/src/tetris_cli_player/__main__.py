acf4de3a3cb29f63a7a6cc4c6c39bf319732f496
Jaxson Pahukula <jaxpahu@gmail.com>
2026-07-07 06:00:40 -1000

    improve rendering

--- packages/cli_player/src/tetris_cli_player/__main__.py
+++ packages/cli_player/src/tetris_cli_player/__main__.py
@@ -1,31 +1,39 @@
 import sys
-import io
 import time
-import os
-if os.name == 'posix':
-    import tty
-    import termios
-elif os.name == 'nt':
-    sys.exit(1)
 
 import tetris_game
-from tetris_game import TetrisGame, TetrisGameBoard, BOARD_WIDTH, BOARD_HEIGHT, BOARD_DISPLAY_HEIGHT
+from tetris_game import TetrisGame, TetrisGameState, BOARD_WIDTH, BOARD_HEIGHT, BOARD_DISPLAY_HEIGHT
 
 from .input.keyboard import start_keyboard_input_daemon, stop_keyboard_input_daemon
 
 COLOR_RESET = "\x1b[0m"
+
+COLOR_FG_RESET = "\x1b[39m"
+COLOR_FG_BLACK = "\x1b[30m"
+COLOR_FG_GRAY = "\x1b[38;5;7m"
+COLOR_FG_WHITE = "\x1b[38;5;15m"
+COLOR_FG_RED = "\x1b[38;5;9m"
+COLOR_FG_GREEN = "\x1b[38;5;10m"
+COLOR_FG_YELLOW = "\x1b[38;5;226m"
+COLOR_FG_BLUE = "\x1b[38;5;12m"
+COLOR_FG_MAGENTA = "\x1b[38;5;13m"
+COLOR_FG_CYAN = "\x1b[38;5;51m"
+COLOR_FG_ORANGE = "\x1b[38;5;208m"
+
+COLOR_BG_RESET = "\x1b[49m"
 COLOR_BG_BLACK = "\x1b[40m"
-COLOR_BG_RED = "\x1b[41m"
-COLOR_BG_GREEN = "\x1b[42m"
-COLOR_BG_YELLOW = "\x1b[43m"
-COLOR_BG_BLUE = "\x1b[44m"
-COLOR_BG_MAGENTA = "\x1b[45m"
-COLOR_BG_CYAN = "\x1b[46m"
-COLOR_BG_WHITE = "\x1b[47m"
+COLOR_BG_GRAY = "\x1b[48;5;7m"
+COLOR_BG_WHITE = "\x1b[48;5;15m"
+COLOR_BG_RED = "\x1b[38;5;9m"
+COLOR_BG_GREEN = "\x1b[48;5;10m"
+COLOR_BG_YELLOW = "\x1b[48;5;226m"
+COLOR_BG_BLUE = "\x1b[48;5;12m"
+COLOR_BG_MAGENTA = "\x1b[48;5;13m"
+COLOR_BG_CYAN = "\x1b[48;5;51m"
 COLOR_BG_ORANGE = "\x1b[48;5;208m"
 
-CELL_COLOR_MAP = {
-    tetris_game.PIECE_NULL: COLOR_RESET,
+PIECE_BG_COLOR_MAP: dict[int, str] = {
+    tetris_game.PIECE_NULL: COLOR_BG_RESET,
     tetris_game.PIECE_I: COLOR_BG_CYAN,
     tetris_game.PIECE_L: COLOR_BG_ORANGE,
     tetris_game.PIECE_J: COLOR_BG_BLUE,
@@ -35,85 +43,98 @@ CELL_COLOR_MAP = {
     tetris_game.PIECE_O: COLOR_BG_YELLOW,
 }
 
-_last_shown_board = None
+PIECE_FG_COLOR_MAP: dict[int, str] = {
+    tetris_game.PIECE_NULL: COLOR_FG_RESET,
+    tetris_game.PIECE_I: COLOR_FG_CYAN,
+    tetris_game.PIECE_L: COLOR_FG_ORANGE,
+    tetris_game.PIECE_J: COLOR_FG_BLUE,
+    tetris_game.PIECE_S: COLOR_FG_GREEN,
+    tetris_game.PIECE_Z: COLOR_FG_RED,
+    tetris_game.PIECE_T: COLOR_FG_MAGENTA,
+    tetris_game.PIECE_O: COLOR_FG_YELLOW,
+}
+
+PIECE_CHAR_MAP: dict[int, str] = {
+    tetris_game.PIECE_NULL: " ",
+    tetris_game.PIECE_I: "I",
+    tetris_game.PIECE_L: "L",
+    tetris_game.PIECE_J: "J",
+    tetris_game.PIECE_S: "S",
+    tetris_game.PIECE_Z: "Z",
+    tetris_game.PIECE_T: "T",
+    tetris_game.PIECE_O: "O",
+}
+
+_last_shown_state = None
 
 
-def visualize_game(game: TetrisGame):
-    global _last_shown_board
+def visualize_state(state: TetrisGameState):
+    global _last_shown_state
 
-    if _last_shown_board is None:
+    if _last_shown_state is None:
         # never been displayed yet
-        _last_shown_board = TetrisGameBoard()
 
         # draw game frame (outline)
-        sys.stdout.write(COLOR_BG_WHITE)
-        sys.stdout.write("  " * (BOARD_WIDTH + 2))
-        sys.stdout.write(COLOR_RESET)
-        sys.stdout.write("\n")
-        for y in reversed(range(BOARD_DISPLAY_HEIGHT)):
-            sys.stdout.write(f"{COLOR_BG_WHITE}  {COLOR_BG_BLACK}")
-            cur_col = COLOR_BG_BLACK
-            for x in range(BOARD_WIDTH):
-                target_col = CELL_COLOR_MAP[game.board.get(x, y)]
-                if cur_col != target_col:
-                    sys.stdout.write(target_col)
-                    cur_col = target_col
-                sys.stdout.write("  ")
-            sys.stdout.write(f"{COLOR_BG_WHITE}  {COLOR_RESET}\n")
-        sys.stdout.write(COLOR_BG_WHITE)
-        sys.stdout.write("  " * (BOARD_WIDTH + 2))
-        sys.stdout.write(COLOR_RESET)
-        sys.stdout.write("\n")
+        sys.stdout.write(f"{COLOR_FG_RESET}{COLOR_BG_RESET} ╷{" " * (BOARD_WIDTH * 2)}╷\n")
+        for y in range(BOARD_DISPLAY_HEIGHT):
+            sys.stdout.write(f" │{" " * (BOARD_WIDTH * 2)}│\n")
+        sys.stdout.write(f" ├{"─" * (BOARD_WIDTH * 2)}┤\n")
+        sys.stdout.write(f" │ Next: {PIECE_FG_COLOR_MAP[state.next_piece]}{PIECE_CHAR_MAP[state.next_piece]}{COLOR_FG_RESET} {" " * ((BOARD_WIDTH * 2) - 10)} │\n")
+        sys.stdout.write(f" │ Score: {state.score:>{(BOARD_WIDTH * 2) - 9}} │\n")
+        sys.stdout.write(f" ╰{"─" * (BOARD_WIDTH * 2)}╯{COLOR_RESET}\n")
+
+        _last_shown_state = state.copy()
     
-    cur_board = game.board.copy()
-    
-    # show falling piece    
-    for block_x, block_y in game.falling_piece_cells():
-        cur_board.set(block_x, block_y, game.current_piece())
+    # show falling piece
+    for block_x, block_y in state.falling_piece_cells:
+        state.set_board(block_x, block_y, state.falling_piece)
     
     # calculate diffs
-    different = False
     row_diffs: list[None | list[tuple[int, int]]] = [None] * BOARD_DISPLAY_HEIGHT
     for y in range(BOARD_DISPLAY_HEIGHT):
         for x in range(BOARD_WIDTH):
             # check for differences
-            if cur_board.get(x, y) != _last_shown_board.get(x, y):
-                different = True
+            if state.get_board(x, y) != _last_shown_state.get_board(x, y):
                 if row_diffs[y] is None:
                     row_diffs[y] = []
-                row_diffs[y].append((x, cur_board.get(x, y)))
+                row_diffs[y].append((x, state.get_board(x, y)))
     
     # draw changed cells
-    cursor_y = -2
-    cursor_x = -1
+    cursor_y = 0
+    cursor_x = 0
     for line, col_diffs in enumerate(row_diffs):
         # skip lines w no changes
         if col_diffs is None:
             continue
         # move up to the correct line
-        sys.stdout.write(f"\x1b[{line - cursor_y}A")
-        cursor_y = line
+        target_cursor_y = 5 + line
+        sys.stdout.write(f"\x1b[{target_cursor_y - cursor_y}A")
+        cursor_y = target_cursor_y
 
         for col, value in col_diffs:
             # move to column and write block
-            if col > cursor_x:
-                sys.stdout.write(f"\x1b[{(col - cursor_x) * 2}C")
-            elif col < cursor_x:
-                sys.stdout.write(f"\x1b[{(cursor_x - col) * 2}D")
-            sys.stdout.write(f"{CELL_COLOR_MAP[value]}  {COLOR_RESET}")
-            cursor_x = col + 1
+            target_cursor_x = 2 + (col * 2)
+            if target_cursor_x > cursor_x:
+                sys.stdout.write(f"\x1b[{target_cursor_x - cursor_x}C")
+            elif target_cursor_x < cursor_x:
+                sys.stdout.write(f"\x1b[{cursor_x - target_cursor_x}D")
+            sys.stdout.write(f"{PIECE_BG_COLOR_MAP[value]}  {COLOR_BG_RESET}")
+            cursor_x = target_cursor_x + 2
+    
+    if state.next_piece != _last_shown_state.next_piece:
+        pass
     
     # reset cursor
-    if cursor_y != -2:
-        sys.stdout.write(f"\x1b[{cursor_y + 2}B")
-    if cursor_x != -1:
+    if cursor_y > 0:
+        sys.stdout.write(f"\x1b[{cursor_y}B")
+    elif cursor_y < 0:
+        sys.stdout.write(f"\x1b[{cursor_y}A")
+    if cursor_x != 0:
         sys.stdout.write("\r")
-
     sys.stdout.flush()
 
-    if different:
-        # remember state
-        _last_shown_board = cur_board.copy()
+    # remember state
+    _last_shown_state = state.copy()
 
 def handle_key(game: TetrisGame, key: bytes):
     if key == b'LEFT':
@@ -122,13 +143,13 @@ def handle_key(game: TetrisGame, key: bytes):
         game.shift_right()
     elif key == b'UP':
         game.rotate_cw()
-    elif key == b'DOWN':
+    elif key == b'Z':
         game.rotate_ccw()
 
 def main():
     try:
         game = TetrisGame()
-        game._piece_bb_y = 10
+        game._falling_piece_bb_y = 10
 
         q = start_keyboard_input_daemon()
 
@@ -138,7 +159,8 @@ def main():
 
             time.sleep(1.0 / 30.0)
             #game._piece_rot = (game._piece_rot + 1) % 4
-            visualize_game(game)
+            state = game.get_state()
+            visualize_state(state)
     except KeyboardInterrupt:
         stop_keyboard_input_daemon()
 
--- packages/game/src/tetris_game/__init__.py
+++ packages/game/src/tetris_game/__init__.py
@@ -1,5 +1,6 @@
 import random
 from collections import deque
+import dataclasses
 
 from .data import PIECE_DATA
 
@@ -18,49 +19,78 @@ PIECE_O = 7
 
 _FULL_SEVEN_BAG = [PIECE_I, PIECE_L, PIECE_J, PIECE_S, PIECE_Z, PIECE_T, PIECE_O]
 
-class TetrisGameBoard:
-    def __init__(self, data: list[int] | None = None):
-        if data is not None and len(data) != BOARD_WIDTH * BOARD_HEIGHT:
-            raise ValueError(f"GameBoard data must be an array of length {BOARD_WIDTH * BOARD_HEIGHT}, got {len(data)}")
-        self._data = [PIECE_NULL] * (BOARD_WIDTH * BOARD_HEIGHT) if data is None else list(data)
+@dataclasses.dataclass
+class TetrisGameState:
+
+    score: int
+    next_piece: int
+
+    falling_piece: int
+    falling_piece_rot: int
+    falling_piece_bb_x: int
+    falling_piece_bb_y: int
+    falling_piece_cells: list[tuple[int, int]]
+
+    board_data: list[int]
+
+    def __post_init__(self):
+        if len(self.board_data) != BOARD_WIDTH * BOARD_HEIGHT:
+            raise ValueError(f"Game board data must be an array of length {BOARD_WIDTH * BOARD_HEIGHT}, got {len(self.board_data)}")
     
-    def get(self, x: int, y: int) -> int:
+    def get_board(self, x: int, y: int) -> int:
         """
         Gets the value of a specific position on the board
         """
-        return self._data[y * BOARD_WIDTH + x]
+        return self.board_data[y * BOARD_WIDTH + x]
 
-    def set(self, x: int, y: int, value: int) -> int:
+    def set_board(self, x: int, y: int, value: int) -> int:
         """
         Sets the value at a specific position on the board, returning the old value
         """
-        old = self._data[y * BOARD_WIDTH + x]
-        self._data[y * BOARD_WIDTH + x] = value
+        old = self.board_data[y * BOARD_WIDTH + x]
+        self.board_data[y * BOARD_WIDTH + x] = value
         return old
 
-    def copy(self) -> 'TetrisGameBoard':
-        return TetrisGameBoard(data=self._data)
+    def copy(self) -> 'TetrisGameState':
+        return dataclasses.replace(self, board_data=list(self.board_data))
 
 class TetrisGame:
     def __init__(self):
 
         # init board
-        self.board = TetrisGameBoard()
+        self._board: list[int] = [PIECE_NULL] * (BOARD_WIDTH * BOARD_HEIGHT)
+
+        self._score: int = 123000
 
         self._piece_queue: deque[int] = deque()
         self._extend_piece_queue()
 
-        self._current_piece = self._piece_queue.popleft()
-        self._piece_rot: int
-        self._piece_bb_x: int
-        self._piece_bb_y: int
+        self._falling_piece = self._piece_queue.popleft()
+        self._falling_piece_rot: int
+        self._falling_piece_bb_x: int
+        self._falling_piece_bb_y: int
         self._place_new_piece()
+    
+    def get_state(self) -> TetrisGameState:
+        if len(self._piece_queue) == 0:
+            self._extend_piece_queue()
+
+        return TetrisGameState(
+            board_data=list(self._board),
+            score=self._score,
+            next_piece=self._piece_queue[0],
+            falling_piece_cells=self.falling_piece_cells(),
+            falling_piece=self._falling_piece,
+            falling_piece_rot=self._falling_piece_rot,
+            falling_piece_bb_x=self._falling_piece_bb_x,
+            falling_piece_bb_y=self._falling_piece_bb_y,
+        )
     
     def falling_piece_cells(self) -> list[tuple[int, int]]:
         """
         Returns the cell positions of all the blocks in the falling piece
         """
-        a = [(x + self._piece_bb_x, y + self._piece_bb_y) for x, y in PIECE_DATA[self._current_piece][self._piece_rot]]
+        a = [(x + self._falling_piece_bb_x, y + self._falling_piece_bb_y) for x, y in PIECE_DATA[self._falling_piece][self._falling_piece_rot]]
         return a
 
     def tick_gravity(self):
@@ -74,7 +104,7 @@ class TetrisGame:
             # checking if any pieces would be out of bounds
             if x <= 0:
                 return
-        self._piece_bb_x -= 1
+        self._falling_piece_bb_x -= 1
 
     def shift_right(self):
         """
@@ -84,21 +114,27 @@ class TetrisGame:
             # checking if any pieces would be out of bounds
             if x >= BOARD_WIDTH - 1:
                 return
-        self._piece_bb_x += 1
+        self._falling_piece_bb_x += 1
     
     def rotate_cw(self):
         """
         Attempts to rotate the falling piece clockwise
         """
         # TODO SRS
-        self._piece_rot = (self._piece_rot + 1) % 4
+        self._falling_piece_rot = (self._falling_piece_rot + 1) % 4
 
     def rotate_ccw(self):
         """
         Attempts to rotate the falling piece counter-clockwise
         """
         # TODO SRS
-        self._piece_rot = (self._piece_rot - 1) % 4
+        self._falling_piece_rot = (self._falling_piece_rot - 1) % 4
+    
+    def soft_drop(self):
+        self._falling_piece_bb_y += 1
+
+    def hard_drop(self):
+        self._falling_piece_bb_y += 2
     
     def current_piece(self) -> int:
         return self._current_piece
@@ -106,9 +142,9 @@ class TetrisGame:
     def _place_new_piece(self):
         """
         """
-        self._piece_rot: int = 0
-        self._piece_bb_x = 3
-        self._piece_bb_y = 22
+        self._falling_piece_rot = 0
+        self._falling_piece_bb_x = 3
+        self._falling_piece_bb_y = 19
     
     def _extend_piece_queue(self):
         bag = list(_FULL_SEVEN_BAG)
